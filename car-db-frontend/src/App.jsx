127caa214093d17a28fca20ae6aa82140ff5800e
Mucngoo <s4029319@rmit.edu.vn>
2025-11-29 14:22:44 +0700

    folder structure

--- car-db-frontend/src/App.jsx
+++ car-db-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./index.css";
+
 import CarListing from "./pages/CarListing";
 import OrderSummary from "./pages/OrderSummary";
 import OrderForm from "./pages/OrderForm";
@@ -10,6 +11,16 @@ import News from "./pages/News";
 import NewsDetail from "./pages/NewsDetail";
 import FAQ from "./pages/FAQ";
 import Homepage from "./pages/Homepage";
+
+import AdminNavbar from './components/AdminNavbar';
+import ApprovedCar from './admin-pages/ApprovedCars';
+import WaitlistCar from './admin-pages/WaitlistCars';
+import BoughtCars from './admin-pages/BoughtCars';
+import ApprovedCarDetail from './admin-pages/ApprovedCarDetail';
+import WaitlistCarDetail from './admin-pages/WaitlistCarDetail';
+import BoughtCarDetail from './admin-pages/BoughtCarDetail';
+import PostNews from './admin-pages/PostNews';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -29,6 +40,18 @@ function App() {
           <Route path="/" element={<Homepage />} />
         </Routes>
       </Navbar>
+
+      <AdminNavbar>
+        <Routes>
+            <Route path="/approved-cars" element={<ApprovedCar />} />
+            <Route path="/waitlist-cars" element={<WaitlistCar />} />
+            <Route path="/bought-cars" element={<BoughtCars />} />
+            <Route path="/approved-car/:id" element={<ApprovedCarDetail />} />
+            <Route path="/waitlist-car/:id" element={<WaitlistCarDetail />} />
+            <Route path="/bought-car/:id" element={<BoughtCarDetail />} />
+            <Route path="/post-news" element={<PostNews />} />
+        </Routes>
+      </AdminNavbar>
     </BrowserRouter>
   );
 }
